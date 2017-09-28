ad21e4676048b1fb683ca5dff678b8b2652abcd8
agusting1994 <agustinguallanes@gmail.com>
2017-09-27 23:47:21 -0300

    test

---
+++ Proyectoxv1/Proyectoxv1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyectoxv1.MvcApplication" Language="C#" %>
---
+++ Proyectoxv1/Proyectoxv1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyectoxv1.Startup))]
+namespace Proyectoxv1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
